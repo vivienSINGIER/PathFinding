1da3f2c2d97a402b5e3b28b1eb90ee9c7d3186c4
Ic3Scout <bcombeau@gaming.tech>
2025-11-10 13:02:39 +0100

    [REF] GridConfig + Draw rectangles

--- src/LightEngine/Debug.h
+++ src/LightEngine/Debug.h
@@ -6,6 +6,7 @@
 #include <SFML/Graphics/Vertex.hpp>
 #include <SFML/Graphics/Text.hpp>
 #include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
 
 class GameManager;
 
@@ -26,6 +27,7 @@ class Debug
 	std::vector<Line> mLines;
 	std::vector<sf::Text> mTexts;
 	std::vector<sf::CircleShape> mCircles;
+	std::vector<sf::RectangleShape> mRectangles;
 
 	void Draw(sf::RenderWindow* pRenderWindow);
 
@@ -34,6 +36,7 @@ public:
 
 	static void DrawLine(float x1, float y1, float x2, float y2, const sf::Color& color);
 	static void DrawRectangle(float x, float y, float width, float height, const sf::Color& color);
+	static void DrawFilledRectangle(float x, float y, float width, float height, const sf::Color& color);
 	static void DrawCircle(float x, float y, float radius, const sf::Color& color);
 	static void DrawText(float x, float y, const std::string& text, const sf::Color& color);
 	static void DrawText(float x, float y, const std::string& text, float ratioX, float ratioY, const sf::Color& color);
